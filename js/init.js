a5ee1afe876537ba84d3960dcaed1771c24f0d20
MrClaus <girosoft@mail.ru>
2016-10-21 17:53:05 +0300

    update

--- js/init.js
+++ js/init.js
@@ -51,8 +51,8 @@ function preLoadingApp() {
 function start() {
 	console.log('3');
 	console.log('Preload is ', i);
-	texture1 = resLoaded('js/ich.jpg');
-	texture = resLoaded(user['photo']);
+	texture1 = imgResLoad('js/ich.jpg');
+	texture = imgResLoad(user['photo']);
 	console.log('9');
 	startGame();	
 }
