cfb2792fc9cf1fa1494cd0e4d11e4ffb09b3320e
MrClaus <girosoft@mail.ru>
2017-02-03 09:58:05 +0300

    test

--- js/intro/intro.js
+++ js/intro/intro.js
@@ -24,7 +24,7 @@ var luna, moon, m_bump, m_specular;
 var skyBox, mapSky;	
 var stats;			
 var composer, effectFilm, effectVignette, glitchPass, effectFilm;			
-var mouseX = 0, mouseY = 0, mouseRad = 999, mouseSt = 0;
+var mouseX = 0, mouseY = 0, mouseRad = 999, mouseSt = 0, process = 0;
 
 var bgi, render2D, scene2D; // для pixi
 
@@ -401,18 +401,29 @@ function renderIntro() {
 	function render() {
 		requestAF(render);		
 		animate(); // код сцены, который исполняется во время рендринга
-		composer.render(0.01);
+		composer.render(0.01); // рендрид 3д слой
 		render2D.render(scene2D); // поверх 3д слоя рендрит 2д слой
 	}
 	
 	function animate() {
 		controls.update();		
 		stats.update();
-		if (mouseRad < 122) {						
-			controls.maxDistance = 3600 + 656 * (mouseRad / 122);
-			controls.minDistance = controls.maxDistance;
+		
+		if (process != 0 && process != 800) {			
+			process = process + mouseSt * (0.5);
+			if (mouseSt == 1 && process >= 800) process = 800;
+			if (mouseSt == -1 && process <= 1) process = 0;
+			controls.maxDistance = 3600 + process;
+			controls.minDistance = controls.maxDistance;					
 		}
-		if (mouseSt != 0) console.log(mouseSt);		
+		
+
+		
+		//if (mouseRad < 122) {						
+		//	controls.maxDistance = 3600 + 656 * (mouseRad / 122);
+		//	controls.minDistance = controls.maxDistance;
+		//}
+		//if (mouseSt != 0) console.log(mouseSt);		
 		sphere.rotation.y += 0.0003;
 		clouds.rotation.y += 0.0006;
 		skyBox.rotation.z += 0.0003;
@@ -434,9 +445,15 @@ function renderIntro() {
 		mouseRad = Math.sqrt(Math.pow(mouseX, 2) + Math.pow(mouseY, 2));
 	}
 	
-	function onDocumentMouseClick( event ) {
-		if (mouseSt == 1) mouseSt = -1;
-		else mouseSt = 1;
+	function onDocumentMouseClick( event ) {		
+		if (mouseSt == 1) {
+			mouseSt = -1;
+			if (process == 800) process = 799;
+		}
+		else {
+			mouseSt = 1;
+			if (process == 0) process = 1;
+		}
 	}
 	
 	// *** Конец неосновного кода ***
