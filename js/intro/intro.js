5fe69f5e59da7db590c730cb1c78796cd2c649b5
MrClaus <girosoft@mail.ru>
2017-07-05 13:42:49 +0300

    rrr

--- js/intro/intro.js
+++ js/intro/intro.js
@@ -376,6 +376,7 @@ function renderIntro() {
 	
 	function onDocumentMouseWheel(event) {
 		distance += event.deltaY * 0.05;
+		console.log('X(',options_particleSystem.position.x,') Y(',options_particleSystem.position.y,') Z(',options_particleSystem.position.z,')');
 	}
 	
 	function ControlsMove() {
