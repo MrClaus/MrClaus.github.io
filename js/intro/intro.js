7e13e499a27297650a4ddd1d524655bfaf36eb30
MrClaus <girosoft@mail.ru>
2017-07-06 19:40:42 +0300

    fgt

--- js/intro/intro.js
+++ js/intro/intro.js
@@ -440,8 +440,8 @@ function renderIntro() {
 		lon += 0.618 * (mouseX - width / 2)/(width / 2);
 		lat += 0.618 * (mouseY - height / 2)/(height / 2);
 		
-		camera.position.x+=lon;
-		camera.position.y+=lat;
+		camera.position.x+=lon*0.01;
+		camera.position.y+=lat*0.01;
 	}
 	
 	// *** Конец неосновного кода ***
