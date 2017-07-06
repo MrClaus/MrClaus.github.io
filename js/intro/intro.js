d73212729b255fe400839f9064ddbf8dc13508c2
MrClaus <girosoft@mail.ru>
2017-07-06 20:44:03 +0300

    tt

--- js/intro/intro.js
+++ js/intro/intro.js
@@ -326,12 +326,12 @@ function initEffectRender() {
 	// добавляем созданные эффекты в композёр, начиная с рендринга сцены
 	// Пробуем добавить размазывающий эффект как ночью в гта3
 	var renderPass = new THREE.RenderPass(scene, camera);
-	renderPass.clear = false;	
+	//renderPass.clear = false;	
 	composerAlpha.addPass(renderPass);
 	composerAlpha.addPass(bloomPass);
 	//composerAlpha.addPass(effectFilm);
 	//var renderScene = new THREE.TexturePass(composerAlpha.renderTarget2.texture);
-	var renderScene = new THREE.TexturePass(renderPass);
+	var renderScene = new THREE.TexturePass(camera);
 	renderScene.opacity = 0.5;
 	
 	
@@ -341,7 +341,7 @@ function initEffectRender() {
 	//composer.addPass(effectFilm);
 	//composer.addPass(new THREE.RenderPass(sceneOrtho, cameraOrtho)); // добавляем 2д слой "геймплэй"
 			
-	//composer.addPass(effectVignette);				
+	composer.addPass(effectVignette);				
 	//composer.addPass(glitchPass);
 	
 	// *** Конец основного кода ***
