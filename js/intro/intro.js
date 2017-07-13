1cb2c5f231c9a763406bba30ec01d528d61627a4
MrClaus <girosoft@mail.ru>
2017-07-13 15:14:55 +0300

    vvvv

--- js/intro/intro.js
+++ js/intro/intro.js
@@ -621,10 +621,11 @@ function renderIntro() {
 		var rad = Math.sqrt(xx*xx + yy*yy + zz*zz);
 		var alfa = Math.acos(zz/rad);
 		var betta = Math.acos(xx/(rad*Math.sin(alfa)));
-		if (xx > 0) betta = 2 * Math.PI - betta;
+		if (xx < 0) betta = 2 * Math.PI - betta;
 		sphereMesh.position.x = 100*Math.sin(alfa)*Math.cos(betta);
 		sphereMesh.position.y = 100*Math.sin(alfa)*Math.sin(betta);
 		sphereMesh.position.z = 100*Math.cos(alfa);
+		console.log(alfa);
 		
 		
 		
