76fd4b51e24451a789f7000337d0d0bace0ae05d
MrClaus <girosoft@mail.ru>
2017-07-13 15:45:05 +0300

    jjj

--- js/intro/intro.js
+++ js/intro/intro.js
@@ -620,9 +620,9 @@ function renderIntro() {
 		var zz = camera.position.z;
 		var rad = Math.sqrt(xx*xx + yy*yy + zz*zz);
 		var alfa = Math.acos(zz/rad);
-		if (zz > 0) alfa = Math.PI - alfa;
+		
 		var betta = Math.acos(xx/(rad*Math.sin(alfa)));
-		if (xx > 0) betta = 2 * Math.PI - betta;
+		if (xx > 0) betta = Math.PI + betta;
 		sphereMesh.position.x = 100*Math.sin(alfa)*Math.cos(betta);
 		sphereMesh.position.y = 100*Math.sin(alfa)*Math.sin(betta);
 		sphereMesh.position.z = 100*Math.cos(alfa);
