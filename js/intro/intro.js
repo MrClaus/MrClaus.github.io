b7fb8b196f2a08048ccaff4143e1a75e69b48bb9
MrClaus <girosoft@mail.ru>
2017-07-21 21:56:14 +0300

    gggg

--- js/intro/intro.js
+++ js/intro/intro.js
@@ -92,7 +92,8 @@ function resLoad() {
 	
 	spriteRes = loadIMG("res/intro/testDevice.png");
 	
-	mipp = loadIMG("mipTest.bmp");
+	//mipp = loadIMG("mipTest.bmp");
+	mipp = THREE.ImageUtils.loadTexture("mipTest.bmp");
 	mipp.minFilter = mipp.magFilter = THREE.LinearFilter;
 	mipp.anisotropy = 4;
 	matt = new THREE.MeshBasicMaterial( { map: mipp } );
