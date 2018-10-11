ca9509ddfc2074046d941b39defa146b07b5cb13
MrClaus <girosoft@mail.ru>
2018-10-11 21:48:53 +0300

    ggg

--- js/intro/frame.js
+++ js/intro/frame.js
@@ -21,10 +21,10 @@ function start() {
 
 function init() {
   var container = document.getElementById('container');
-  var width = 950; //window.innerWidth || 1;
-  var height = 190; //window.innerHeight || 1;
-  //var width = container.offsetWidth;
-  //var height = container.offsetHeight;
+  //var width = 950; //window.innerWidth || 1;
+  //var height = 190; //window.innerHeight || 1;
+  var width = container.getBoundingClientRect().width;
+  var height = container.getBoundingClientRect().height;
   //var aspect = width / height;
   var devicePixelRatio = window.devicePixelRatio || 1;
   renderer = new THREE.WebGLRenderer();
