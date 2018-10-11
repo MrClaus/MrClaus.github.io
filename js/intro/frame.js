40c1347c17fb843ac3e13d12023593d07a6a333f
MrClaus <girosoft@mail.ru>
2018-10-12 00:29:22 +0300

    klk

--- js/intro/frame.js
+++ js/intro/frame.js
@@ -20,7 +20,7 @@ function start() {
 
 
 function init() {
-  var container = document.getElementById('cul');
+  var container = document.getElementById('container');
   //var width = 950; //window.innerWidth || 1;
   //var height = 190; //window.innerHeight || 1;
   var width = $( container ).width();
